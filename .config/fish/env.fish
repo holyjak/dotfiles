c244d3441696d6f9ce6e5602edb786d79991d855
Jakub Holy <jakub.holy@iterate.no>
2015-04-18 20:44:03 +0200

    Replace brew --prefix with values; time ~200ms->20ms

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,12 +1,14 @@
-set -x PATH (brew --prefix coreutils)/libexec/gnubin $PATH
+## Note: Replaced calls to brew --prefix with actual values since
+## it is slow (~ 200 ms)
+set -x PATH /usr/local/opt/libexec/gnubin $PATH
 
 ### RBENV
-set -x RBENV_ROOT (brew --prefix rbenv)
+set -x RBENV_ROOT /usr/local/opt/rbenv
 set -x PATH $RBENV_ROOT/bin $PATH
 set -x PATH $RBENV_ROOT/shims $PATH
 
-set -x GEM_HOME (brew --prefix)/opt/gems
-set -x GEM_PATH (brew --prefix)/opt/gems
+set -x GEM_HOME /usr/local/opt/gems
+set -x GEM_PATH /usr/local/opt/gems
 
 ### NVM
-source ~/.config/fish/nvm-wrapper/nvm.fish
+#source ~/.config/fish/nvm-wrapper/nvm.fish
