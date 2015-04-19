e763e3f518c7562990cad12b5fc89030fb5c9d74
Jakub Holy <jakub.holy@iterate.no>
2015-04-19 23:46:21 +0200

    Add manually path to nvm bin/

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,6 +1,6 @@
 ## Note: Replaced calls to brew --prefix with actual values since
-## it is slow (~ 200 ms)
-set -x PATH /usr/local/opt/libexec/gnubin $PATH
+## it is slow (~ 200 ms) (min. b. -p. coreutils; b. -p itsel is quick)
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 
 ### RBENV
 set -x RBENV_ROOT /usr/local/opt/rbenv
@@ -12,3 +12,4 @@ set -x GEM_PATH /usr/local/opt/gems
 
 ### NVM
 #source ~/.config/fish/nvm-wrapper/nvm.fish
+set -x PATH $PATH ~/.nvm/v0.10.38/bin
