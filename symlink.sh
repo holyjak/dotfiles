d80ff3ad3c0aed1f944dbb268fa732162ed5f402
Jakub Holy <jakub.holy@teliasonera.com>
2016-01-19 08:13:55 +0100

    Enable ligatures in Intellij 15
    to make it work with FireCoda

--- symlink.sh
+++ symlink.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
+# Symlink the dotfiles to their target places
+
+function symlink {
+  local RELATIVE_PATH=$1
+  local INCLUDE_RE=$2
+  [ -z "$INCLUDE_RE" ] && INCLUDE_RE=".*"
+  local EXCLUDE_RE=$3
+  [ -n "$EXCLUDE_RE" ] && EXCLUDE_RE="$EXCLUDE_RE|"
+  local SRC_DIR=~/dotfiles/$RELATIVE_PATH
+  local DST_DIR=~/$RELATIVE_PATH
+  echo; echo ">> Symlinking $SRC_DIR (excluding $EXCLUDE_RE)"
+  [ -d "$DST_DIR" ] || mkdir -p "$DST_DIR"
+  for F in $(ls -aA $SRC_DIR | egrep "^($INCLUDE_RE)$" | egrep -v "^($EXCLUDE_RE\.|\.\.|\.DS_Store)$"); do
+    ln -shi "$SRC_DIR/$F" "$DST_DIR/$F"
+  done
+}
+
 DOTFILES_DIR=$(dirname $0)
 cd $DOTFILES_DIR
-echo ">>> Symlinking files from $(pwd) to ~/"
-for F in $(ls -aA | egrep "^\.[^.]+" | egrep -v "\.config$|\.git$"); do
-  ln -shi ~/dotfiles/$F ~/$F
-done
 
-mkdir -p ~/.config/fish
+symlink . "\.[^.]+" "\.config|\.git"
+
+symlink ".config/fish" ".*" "user\.fish"
 ln -s -i ~/dotfiles/.config/fish/user.fish ~/.config/fish/${USER}.fish
-for F in $(ls .config/fish/ | egrep -v "user\.fish"); do 
-  ln -shi ~/dotfiles/.config/fish/$F ~/.config/fish/$F
-done
 
-echo ">> Installing Bass"
+symlink Library/LaunchAgents
+
+symlink Library/Preferences/IntelliJIdea15/
+
+echo ">> Installing Bass (bash wrapper for fish to use nvm etc.)"
 cd ${DOTFILES_DIR}/bass-bash4shell-wrapper && make install
+
