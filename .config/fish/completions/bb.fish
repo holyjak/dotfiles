b29e81de6488b534a948929334011feaf986ba67
Jakub Holy <jakubholy@jakubholy.net>
2021-09-02 13:40:20 +0200

    fix bb task completions not to be static

--- .config/fish/completions/bb.fish
+++ .config/fish/completions/bb.fish
@@ -1,9 +1,10 @@
 function __bb_complete_tasks
-  if not test "$__bb_tasks"
-    set -g __bb_tasks (bb tasks |tail -n +3 |cut -f1 -d ' ')
-  end
+  # if not test "$__bb_tasks" # BAD: Sets for all future executions anywhere
+  #   set -g __bb_tasks (bb tasks |tail -n +3 |cut -f1 -d ' ')
+  # end
+  set -l __bb_tasks (bb tasks |tail -n +3 |cut -f1 -d ' ')
 
   printf "%s\n" $__bb_tasks
 end
 
-complete -c bb -a "(__bb_complete_tasks)" -d 'tasks'
+complete -c bb -a "(__bb_complete_tasks)" --description 'tasks'
